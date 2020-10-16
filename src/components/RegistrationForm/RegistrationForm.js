f69d4d1c95058ac9e598c0ed9a462b682e163042
Tiff Joy <tiffanysummerford@gmail.com>
2020-10-16 10:48:31 -0400

    changed reg page to match login

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import UserContext from '../../contexts/UserContext'
 import AuthApiService from '../../services/auth-api-service';
+import './RegistrationForm.css'
 
 export default class Registration extends React.Component {
   static contextType = UserContext;
@@ -28,45 +29,36 @@ export default class Registration extends React.Component {
 
   render() {
     return (
-      <main className="flex flex-col center flex-1 width-full">
+      <main className="mainBody">
 
         {
           this.state.error &&
           <div className='error'>{this.state.error}</div>
         }
 
-        <div className="flex-1 flex  center p-2 mt-4 justify-stretch">
+          <form className="form" onSubmit={this.handleRegister}>
 
-          <form className="boxshadow rounded-lighter flex-1 flex-grow flex flex-col center  m-2" onSubmit={this.handleRegister}>
-
-            <div className="flex flex-row flex-1 mx-2 max-width-80 font-lg">
-              <label htmlFor="useremail" className="flex-1">Email Address</label>
-              <input type="email" name="useremail" id="useremail"
-                className="flex-1 width-full font-lg border-none border-b" />
+            <div className="row">
+              <label htmlFor="useremail">Email Address</label>
+              <input type="email" name="useremail" id="useremail"/>
             </div>
 
-            <div className="flex flex-row flex-1 mx-2 max-width-80 font-lg">
-              <label htmlFor="username" className="flex-1">User Name</label>
-              <input type="text" name="username" id='username'
-                className="flex-1 width-full font-lg border-none border-b" />
+            <div className="row">
+              <label htmlFor="username">User Name</label>
+              <input type="text" name="username" id='username'/>
             </div>
 
-            <div className="flex flex-row flex-1 mx-2 max-width-80 font-lg">
-              <label htmlFor="userpass" className="flex-1">Password</label>
-              <input type="password" name="userpass" id="userpass"
-                className="flex-1 width-full font-lg border-none border-b" />
+            <div className="row">
+              <label htmlFor="userpass">Password</label>
+              <input type="password" name="userpass" id="userpass"/>
             </div>
 
-            <div className="flex-1 flex flex-row-wrap justify-around">
-              <button className="flex-1 m-1 boxshadow-light border-none rounded-lighter">Register</button>
-              <button className="flex-1 m-1 boxshadow-light border-none rounded-lighter">Login</button>
-              <button className="flex-2 m-1 boxshadow-light border-none rounded-lighter">Login With Facebook</button>
+            <div>
+              <button>Register</button>
+              <button>Login With Facebook</button>
             </div>
-              <Link to='/login'>
-                <a>Already have an account?</a>
-              </Link>
+              <Link className="accountRte" to='/login'>Already have an account?</Link>
           </form>
-        </div>
       </main>
     )
   }
