fcc98cb2a1d37b4038bb302fd7273b073492e120
Tiff Joy <tiffanysummerford@gmail.com>
2020-10-26 12:56:31 -0400

    cssss

--- src/routes/About/About.js
+++ src/routes/About/About.js
@@ -3,7 +3,6 @@ import Accordion from '../../components/AccordionBtn/AccordionBtn';
 import ReactTooltip from 'react-tooltip';
 import { Link } from 'react-router-dom'
 import './About.css'
-import '../../App.css'
 
 export default function About() {
 
@@ -11,51 +10,65 @@ export default function About() {
   const upperNum=`If you can do this PLEASE send us an email because we haven't seen one yet!`
   let aboutData = [
     {
-      header: 'What is Endless Encounters?',
+      header: (
+        <h3 className='question'>
+          What is Endless Encounters?
+        </h3>
+        ),
       content: (  
         <div>
         <ReactTooltip id="rpg" place="top" effect="solid" getContent={(dataTip) => `${dataTip}`} />
-        Endless Encounters is a text-based {<div data-event="click" clickable="true" data-tip="Role Playing Game"
+        Endless Encounters is a text-based {<div className='dataTip'  data-event="click" clickable="true" data-tip="Role Playing Game"
           data-for="rpg">RPG</div>} developed by Emotionally Exhausted. Once you're in, you can explore in search of ravenous enemies to duel swords with or visit the tavern to discuss rumors.
         </div>
       )
     },
     {
-      header: 'How do I sign up?',
+      header: (
+        <h3 className='question'>
+          How do I sign up?
+        </h3>
+        ),
       content:(
       <div>
-      <p>Creating an account is simple:</p>
-        <ol>
-          <li>Click the register link above or {<Link to="/register" >here</Link>}</li>
-          <li>Enter a fun and unique Username</li>
-          <li>Enter a memorable password with at least one of each:
-        <ul className="list-style-none p-1 font-sm center p-0">
-              <li className="text-left ">An upper case Letter</li>
-              <li className="text-left ">A lower case Letter</li>
-              <li className="text-left ">A number</li>
-              <li className="cursor-pointer text-dec-none text-blue font-bold text-left" data-event="click"
+        <ReactTooltip id="special-characters" place="top" effect="solid" getContent={(dataTip) => `${dataTip}`} />
+        <ReactTooltip id="uppercaseNum" place="top" effect="solid" getContent={(dataTip) => `${dataTip}`} />
+          <p>Creating an account is simple:</p>
+            <ol>
+              <li>Click the register link above or {<Link to="/register" >here</Link>}</li>
+              <li>Enter a fun and unique Username</li>
+              <li>Enter a memorable password with at least one of each:
+            <ul>
+              <li  className="dataTip" data-event="click"
                 clickable="true" data-tip={specials}
                 data-for="special-characters">A special character</li>
-              <li className="cursor-pointer text-dec-none text-blue font-bold text-left" data-event="click"
+              <li>An upper case letter</li>
+              <li>A lower case letter</li>
+              <li>A number</li>
+              <li  className="dataTip" data-event="click"
                 clickable="true" data-tip={upperNum} data-for="uppercaseNum">
                 Bonus points for upper/lower case numbers</li>
             </ul>
           </li>
-          <li>Enter an email that you actually use</li>
-          <li>Wait for a verification Email to arrive in your inbox</li>
-          <li>Click verification link in the Email</li>
-          <li>Navigate back to Endless Encounters, log in, and have fun!</li>
-        </ol>
-      </div>)
+              <li>Enter an email that you actually use</li>
+              <li>Wait for a verification Email to arrive in your inbox</li>
+              <li>Click verification link in the Email</li>
+              <li>Navigate back to Endless Encounters, log in, and have fun!</li>
+            </ol>
+      </div>
+      )
     },
     {
-      header: 'How Do I Create my Character?',
+      header: (
+        <h3 className='question'>
+          How Do I Create my Character?
+        </h3>
+      ),
       content: (
         <div>
-         <ReactTooltip id="something" place="top" effect="solid" getContent={(dataTip) => `${dataTip}`} />
               <p>Upon completing your registration, and logging in you'll be pushed to the user dashboard where you'll be prompted to create your first character. </p>
               <ol>
-                <li>Give your character a name (all Alpha-Numeric Characters are valid for this).</li>
+                <li>Give your character a name (all Alpha-Numeric Characters are valid).</li>
                 <li> Assign your character's stat-points (be sure to add some points to each attribute). </li>
                 <li>You may choose from several abilities when you first begin your journey. </li>
                 <li>Clicking on the 'Finalize Character Button' will create your character and you may only have one active character (per-account) at a time.</li>
@@ -64,10 +77,13 @@ export default function About() {
       )
     },
     {
-      header: 'Navigating',
+      header: (
+        <h3 className='question'>
+          Navigating
+        </h3>
+        ),
       content: (
         <div>
-          <ReactTooltip id="Navigating" place="top" effect="solid" getContent={(dataTip) => `${dataTip}`} />
             <p>After creating your character, you'll immediately be enveloped with Endless Encounters.</p>
               <ul>
                 <li>On the dash, you'll be presented with some action choices in the middle-left portion of your dashboard (the 'explore' and 'tavern' buttons).</li>
@@ -79,22 +95,31 @@ export default function About() {
       )
     },
     {
-      header: 'Combat',
+      header: (
+        <h3 className='question'>
+          Combat
+        </h3>
+        ),
       content: (
         <div>
           <ReactTooltip id="Combat" place="top" effect="solid" getContent={(dataTip) => `${dataTip}`} />
             <p>Combat is turn-based; on your turn, you may click on one of your character's abilities or spells' to perform against your target. Your character will then attempt the action, with the chance to miss, hit, or completely obliterate their enemy. Each hit to your opponent will result in damage to their health. Abilities and Spells have cost, so make sure you have enough (health, mana, and ability) to perform this action. Keep a heads up for 
-            {<div data-event="click" clickable="true" data-tip="Critical misses happen if your character rolls 1, critical hits on a roll of 20." data-for="Combat">Critical</div>} misses or hits. </p>
+            {<div className='dataTip'  data-event="click" clickable="true" data-tip="Critical misses happen if your character rolls 1, critical hits on a roll of 20." data-for="Combat"> Critical </div>} misses or hits. </p>
         </div>
       )
     },
     {
-      header: 'Progression',
+      header: (
+        <h3 className='question'>
+          Progression
+        </h3>
+        ),
       content: (
         <div>
           <ReactTooltip id="something" place="top" effect="solid" getContent={(dataTip) => `${dataTip}`} />
           Some text to explain {<div data-event="click" clickable="true"
           data-tip="Brief explain"
+          className='dataTip' 
           data-for="something">something to highlight</div>} more text here
         </div>
       )
@@ -117,8 +142,8 @@ export default function About() {
 
   return(
     <body className="abtBody">
-        <h1 className="font-xxl font-nerd">Endless Encounters</h1>
-        <h3 className="font-lg font-nels">A place for those seeking a way to leave their world behind...</h3>
+        <h1 className='title'>Endless Encounters</h1>
+        <h3 className='subTitle'>A place for those seeking a way to leave their world behind...</h3>
           <section className='abtSect'>
             {aboutLoop()}
           </section> 
